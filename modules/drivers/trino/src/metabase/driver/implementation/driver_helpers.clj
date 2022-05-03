f018d58a1e68bf47e746907ba8b9e3df3f23a4b6
Andrew DiBiasio <andrew.dibiasio@starburstdata.com>
2022-05-03 12:15:16 -0400

    Remove deprecated current-db-time methods

--- modules/drivers/trino/src/metabase/driver/implementation/driver_helpers.clj
+++ modules/drivers/trino/src/metabase/driver/implementation/driver_helpers.clj
@@ -1,25 +1,12 @@
 (ns metabase.driver.implementation.driver-helpers
   "Driver api implementation for Trino JDBC driver."
-  (:require [metabase.driver :as driver]
-            [metabase.driver.common :as driver.common]))
+  (:require [metabase.driver :as driver]))
 
 ;;; Trino API helpers
 
 (defmethod driver/db-start-of-week :trino
   [_]
   :monday)
-
-(defmethod driver.common/current-db-time-date-formatters :trino
-  [_]
-  (driver.common/create-db-time-formatters "yyyy-MM-dd'T'HH:mm:ss.SSSZ"))
-
-(defmethod driver.common/current-db-time-native-query :trino
-  [_]
-  "select to_iso8601(current_timestamp)")
-
-(defmethod driver/current-db-time :trino
-  [& args]
-  (apply driver.common/current-db-time args))
 
 (doseq [[feature supported?] {:set-timezone                    true
                               :basic-aggregations              true
