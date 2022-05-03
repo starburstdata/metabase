7b0631768c43ab3e73a9f590777f4819e05718ce
Andrew DiBiasio <andrew.dibiasio@starburstdata.com>
2022-05-03 12:42:19 -0400

    Small execute comment changes

--- modules/drivers/trino/src/metabase/driver/implementation/execute.clj
+++ modules/drivers/trino/src/metabase/driver/implementation/execute.clj
@@ -30,8 +30,9 @@
 
 (defmethod sql-jdbc.execute/connection-with-timezone :trino
   [driver database ^String timezone-id]
-  ;; Presto supports setting the session timezone via a `TrinoConnection` instance method. Under the covers,
-  ;; this is equivalent to the `X-Presto-Time-Zone` header in the HTTP request (i.e. the `:presto` driver)
+  ;; Trino supports setting the session timezone via a `TrinoConnection` instance method. Under the covers,
+  ;; https://github.com/trinodb/trino/blob/master/client/trino-jdbc/src/main/java/io/trino/jdbc/TrinoConnection.java#L596
+  ;; this is equivalent to the `X-Trino-Time-Zone` header in the HTTP request (i.e. the `:trino` driver)
   (let [conn            (.getConnection (sql-jdbc.execute/datasource-with-diagnostic-info! driver database))
         underlying-conn (pooled-conn->trino-conn conn)]
     (try
@@ -42,12 +43,16 @@
       (try
         (.setReadOnly conn true)
         (catch Throwable e
-          (log/debug e (trs "Error setting connection to read-only"))))
+          (log/warn e (trs "Error setting Trino connection to read-only"))))
       ;; as with statement and prepared-statement, cannot set holdability on the connection level
       conn
       (catch Throwable e
         (.close conn)
         (throw e)))))
+
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;; |                                          Reading Columns from Result Set                                       |
+;;; +----------------------------------------------------------------------------------------------------------------+
 
 (defmethod sql-jdbc.execute/read-column-thunk [:trino Types/TIMESTAMP]
   [_ ^ResultSet rset _ ^Integer i]
@@ -69,122 +74,6 @@
             (t/local-date-time t)
             :else
             t))))))
-
-;; (defmethod sql-jdbc.execute/read-column-thunk [:trino Types/TIMESTAMP]
-;;   [_ ^ResultSet rset _ ^long i]
-;;   (println "Custom Thunk Types/TIMESTAMP...")
-;;   (fn []
-;;     (let [t (.getObject rset i java.sql.Timestamp)
-;;           instant (.toInstant t)]
-;;       (.atOffset instant (t/zone-offset)))))
-
-(defmethod sql-jdbc.execute/read-column-thunk [:trino Types/TIMESTAMP_WITH_TIMEZONE]
-  [_ ^ResultSet rset _ ^long i]
-  (fn []
-    (let [t (.getObject rset i java.sql.Timestamp)
-          instant (.toInstant t)]
-      (.atOffset instant (t/zone-offset)))))
-
-(defn- ^LocalTime sql-time->local-time
-  "Converts the given instance of `java.sql.Time` into a `java.time.LocalTime`, including milliseconds. Needed for
-  similar reasons as `set-time-param` above."
-  [^Time sql-time]
-  ;; Java 11 adds a simpler `ofInstant` method, but since we need to run on JDK 8, we can't use it
-  ;; https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/time/LocalTime.html#ofInstant(java.time.Instant,java.time.ZoneId)
-  (let [^LocalTime lt (t/local-time sql-time)
-        ^Long millis  (mod (.getTime sql-time) 1000)]
-    (.with lt ChronoField/MILLI_OF_SECOND millis)))
-
-(defmethod sql-jdbc.execute/read-column-thunk [:trino Types/TIME_WITH_TIMEZONE]
-  [_ rs _ i]
-  (fn []
-    (let [local-time (-> (.getTime rs i)
-                         sql-time->local-time)]
-      (t/offset-time
-       local-time
-       (t/zone-offset 0)))))
-
-
-(defmethod sql-jdbc.execute/prepared-statement :trino
-  [driver ^Connection conn ^String sql params]
-  ;; with Presto JDBC driver, result set holdability must be HOLD_CURSORS_OVER_COMMIT
-  ;; defining this method simply to omit setting the holdability
-  (let [stmt (.prepareStatement conn
-                                sql
-                                ResultSet/TYPE_FORWARD_ONLY
-                                ResultSet/CONCUR_READ_ONLY)]
-    (try
-      (try
-        (.setFetchDirection stmt ResultSet/FETCH_FORWARD)
-        (catch Throwable e
-          (log/debug e (trs "Error setting prepared statement fetch direction to FETCH_FORWARD"))))
-      (sql-jdbc.execute/set-parameters! driver stmt params)
-      stmt
-      (catch Throwable e
-        (.close stmt)
-        (throw e)))))
-
-
-(defmethod sql-jdbc.execute/statement :trino
-  [_ ^Connection conn]
-  ;; and similarly for statement (do not set holdability)
-  (let [stmt (.createStatement conn
-                               ResultSet/TYPE_FORWARD_ONLY
-                               ResultSet/CONCUR_READ_ONLY)]
-    (try
-      (.setFetchDirection stmt ResultSet/FETCH_FORWARD)
-      (catch Throwable e
-        (log/debug e (trs "Error setting statement fetch direction to FETCH_FORWARD"))))
-    stmt))
-
-(defn- date-time->substitution [ts-str]
-  (sql.params.substitution/make-stmt-subs "from_iso8601_timestamp(?)" [ts-str]))
-
-(defmethod sql.params.substitution/->prepared-substitution [:trino ZonedDateTime]
-  [_ ^ZonedDateTime t]
-  ;; for native query parameter substitution, in order to not conflict with the `PrestoConnection` session time zone
-  ;; (which was set via report time zone), it is necessary to use the `from_iso8601_timestamp` function on the string
-  ;; representation of the `ZonedDateTime` instance, but converted to the report time zone
-  #_(date-time->substitution (.format (t/offset-date-time (t/local-date-time t) (t/zone-offset 0)) DateTimeFormatter/ISO_OFFSET_DATE_TIME))
-  (let [report-zone       (qp.timezone/report-timezone-id-if-supported :trino)
-        ^ZonedDateTime ts (if (str/blank? report-zone) t (t/with-zone-same-instant t (t/zone-id report-zone)))]
-    ;; the `from_iso8601_timestamp` only accepts timestamps with an offset (not a zone ID), so only format with offset
-    (date-time->substitution (.format ts DateTimeFormatter/ISO_OFFSET_DATE_TIME))))
-
-(defmethod sql.params.substitution/->prepared-substitution [:trino LocalDateTime]
-  [_ ^LocalDateTime t]
-  ;; similar to above implementation, but for `LocalDateTime`
-  ;; when Presto parses this, it will account for session (report) time zone
-  (date-time->substitution (.format t DateTimeFormatter/ISO_LOCAL_DATE_TIME)))
-
-(defmethod sql.params.substitution/->prepared-substitution [:trino OffsetDateTime]
-  [_ ^OffsetDateTime t]
-  ;; similar to above implementation, but for `ZonedDateTime`
-  ;; when Presto parses this, it will account for session (report) time zone
-  (date-time->substitution (.format t DateTimeFormatter/ISO_OFFSET_DATE_TIME)))
-
-(defn- set-time-param
-  "Converts the given instance of `java.time.temporal`, assumed to be a time (either `LocalTime` or `OffsetTime`)
-  into a `java.sql.Time`, including milliseconds, and sets the result as a parameter of the `PreparedStatement` `ps`
-  at index `i`."
-  [^PreparedStatement ps ^Integer i ^Temporal t]
-  ;; for some reason, `java-time` can't handle passing millis to java.sql.Time, so this is the most straightforward way
-  ;; I could find to do it
-  ;; reported as https://github.com/dm3/clojure.java-time/issues/74
-  (let [millis-of-day (.get t ChronoField/MILLI_OF_DAY)]
-    (.setTime ps i (Time. millis-of-day))))
-
-(defmethod sql-jdbc.execute/set-parameter [:trino OffsetTime]
-  [_ ^PreparedStatement ps ^Integer i t]
-  ;; necessary because `PrestoPreparedStatement` does not implement the `setTime` overload having the final `Calendar`
-  ;; param
-  (let [adjusted-tz (t/with-offset-same-instant t (t/zone-offset 0))]
-    (set-time-param ps i adjusted-tz)))
-
-(defmethod sql-jdbc.execute/set-parameter [:trino LocalTime]
-  [_ ^PreparedStatement ps ^Integer i t]
-  ;; same rationale as above
-  (set-time-param ps i t))
 
 (defn- ^LocalTime sql-time->local-time
   "Converts the given instance of `java.sql.Time` into a `java.time.LocalTime`, including milliseconds. Needed for
@@ -214,3 +103,122 @@
            (t/zone-offset 0))
           ;; else the base-type is time without time zone, so just return the local-time value
           local-time)))))
+
+(defmethod sql-jdbc.execute/read-column-thunk [:trino Types/TIMESTAMP_WITH_TIMEZONE]
+  [_ ^ResultSet rset _ ^long i]
+  (fn []
+    (let [t (.getObject rset i java.sql.Timestamp)
+          instant (.toInstant t)]
+      (.atOffset instant (t/zone-offset)))))
+
+(defn- ^LocalTime sql-time->local-time
+  "Converts the given instance of `java.sql.Time` into a `java.time.LocalTime`, including milliseconds. Needed for
+  similar reasons as `set-time-param` above."
+  [^Time sql-time]
+  ;; Java 11 adds a simpler `ofInstant` method, but since we need to run on JDK 8, we can't use it
+  ;; https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/time/LocalTime.html#ofInstant(java.time.Instant,java.time.ZoneId)
+  (let [^LocalTime lt (t/local-time sql-time)
+        ^Long millis  (mod (.getTime sql-time) 1000)]
+    (.with lt ChronoField/MILLI_OF_SECOND millis)))
+
+(defmethod sql-jdbc.execute/read-column-thunk [:trino Types/TIME_WITH_TIMEZONE]
+  [_ rs _ i]
+  (fn []
+    (let [local-time (-> (.getTime rs i)
+                         sql-time->local-time)]
+      (t/offset-time
+       local-time
+       (t/zone-offset 0)))))
+
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;; |                                          SQL Statment Operations                                               |
+;;; +----------------------------------------------------------------------------------------------------------------+
+
+(defmethod sql-jdbc.execute/prepared-statement :trino
+  [driver ^Connection conn ^String sql params]
+  ;; with Trino JDBC driver, result set holdability must be HOLD_CURSORS_OVER_COMMIT
+  ;; defining this method simply to omit setting the holdability
+  (let [stmt (.prepareStatement conn
+                                sql
+                                ResultSet/TYPE_FORWARD_ONLY
+                                ResultSet/CONCUR_READ_ONLY)]
+    (try
+      (try
+        (.setFetchDirection stmt ResultSet/FETCH_FORWARD)
+        (catch Throwable e
+          (log/debug e (trs "Error setting prepared statement fetch direction to FETCH_FORWARD"))))
+      (sql-jdbc.execute/set-parameters! driver stmt params)
+      stmt
+      (catch Throwable e
+        (.close stmt)
+        (throw e)))))
+
+
+(defmethod sql-jdbc.execute/statement :trino
+  [_ ^Connection conn]
+  ;; and similarly for statement (do not set holdability)
+  (let [stmt (.createStatement conn
+                               ResultSet/TYPE_FORWARD_ONLY
+                               ResultSet/CONCUR_READ_ONLY)]
+    (try
+      (.setFetchDirection stmt ResultSet/FETCH_FORWARD)
+      (catch Throwable e
+        (log/debug e (trs "Error setting statement fetch direction to FETCH_FORWARD"))))
+    stmt))
+
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;; |                                          Prepared Statement Substitutions                                      |
+;;; +----------------------------------------------------------------------------------------------------------------+
+
+(defn- date-time->substitution [ts-str]
+  (sql.params.substitution/make-stmt-subs "from_iso8601_timestamp(?)" [ts-str]))
+
+(defmethod sql.params.substitution/->prepared-substitution [:trino ZonedDateTime]
+  [_ ^ZonedDateTime t]
+  ;; for native query parameter substitution, in order to not conflict with the `TrinoConnection` session time zone
+  ;; (which was set via report time zone), it is necessary to use the `from_iso8601_timestamp` function on the string
+  ;; representation of the `ZonedDateTime` instance, but converted to the report time zone
+  #_(date-time->substitution (.format (t/offset-date-time (t/local-date-time t) (t/zone-offset 0)) DateTimeFormatter/ISO_OFFSET_DATE_TIME))
+  (let [report-zone       (qp.timezone/report-timezone-id-if-supported :trino)
+        ^ZonedDateTime ts (if (str/blank? report-zone) t (t/with-zone-same-instant t (t/zone-id report-zone)))]
+    ;; the `from_iso8601_timestamp` only accepts timestamps with an offset (not a zone ID), so only format with offset
+    (date-time->substitution (.format ts DateTimeFormatter/ISO_OFFSET_DATE_TIME))))
+
+(defmethod sql.params.substitution/->prepared-substitution [:trino LocalDateTime]
+  [_ ^LocalDateTime t]
+  ;; similar to above implementation, but for `LocalDateTime`
+  ;; when Presto parses this, it will account for session (report) time zone
+  (date-time->substitution (.format t DateTimeFormatter/ISO_LOCAL_DATE_TIME)))
+
+(defmethod sql.params.substitution/->prepared-substitution [:trino OffsetDateTime]
+  [_ ^OffsetDateTime t]
+  ;; similar to above implementation, but for `ZonedDateTime`
+  ;; when Presto parses this, it will account for session (report) time zone
+  (date-time->substitution (.format t DateTimeFormatter/ISO_OFFSET_DATE_TIME)))
+
+;;; +----------------------------------------------------------------------------------------------------------------+
+;;; |                                          Prepared Statement Set Parameters                                     |
+;;; +----------------------------------------------------------------------------------------------------------------+
+
+(defn- set-time-param
+  "Converts the given instance of `java.time.temporal`, assumed to be a time (either `LocalTime` or `OffsetTime`)
+  into a `java.sql.Time`, including milliseconds, and sets the result as a parameter of the `PreparedStatement` `ps`
+  at index `i`."
+  [^PreparedStatement ps ^Integer i ^Temporal t]
+  ;; for some reason, `java-time` can't handle passing millis to java.sql.Time, so this is the most straightforward way
+  ;; I could find to do it
+  ;; reported as https://github.com/dm3/clojure.java-time/issues/74
+  (let [millis-of-day (.get t ChronoField/MILLI_OF_DAY)]
+    (.setTime ps i (Time. millis-of-day))))
+
+(defmethod sql-jdbc.execute/set-parameter [:trino OffsetTime]
+  [_ ^PreparedStatement ps ^Integer i t]
+  ;; necessary because `PrestoPreparedStatement` does not implement the `setTime` overload having the final `Calendar`
+  ;; param
+  (let [adjusted-tz (t/with-offset-same-instant t (t/zone-offset 0))]
+    (set-time-param ps i adjusted-tz)))
+
+(defmethod sql-jdbc.execute/set-parameter [:trino LocalTime]
+  [_ ^PreparedStatement ps ^Integer i t]
+  ;; same rationale as above
+  (set-time-param ps i t))
